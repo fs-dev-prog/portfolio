20f554c33955b30d282112137b547c417d24cf0c
fs-dev-prog <jason19960823@outlook.com>
2025-10-06 05:49:40 -0700

    Allow output into export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
  
 module.exports = {
+  output: 'export', // Enables static export mode
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
@@ -23,5 +24,4 @@ module.exports = {
       },
     ],
   },
-  // output: 'export', // Enables static export mode
 }
